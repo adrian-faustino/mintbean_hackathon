e81a40c48ec8337e69084dad35fb21584d9bdb5a
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-06-22 12:01:26 -0400

    deploying

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -16,12 +17,21 @@ app.use(express.json());
 /** Routes **/
 const routes = require('./routes');
 const testRoutes = require('./routes/testRoutes');
-app.use('/', routes);
-app.use('/test', testRoutes);
+app.use('/api', routes);
+app.use('/api/test', testRoutes);
 
 /** For Heroku Deployment **/
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(
+      __dirname,
+      'client',
+      'build',
+      'index.html'
+    ));
+  });
 };
 
 app.listen(PORT, console.log(`Listening on port ${PORT}...`));
